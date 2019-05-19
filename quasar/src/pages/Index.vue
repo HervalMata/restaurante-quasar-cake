6f2dc3fad0c7fca715d0f249754353dc6ce89bcf
HervalMata <hervalmataamparo@gmail.com>
2019-05-19 16:11:36 -0300

    Pagina inicial responsiva

--- quasar/src/pages/Index.vue
+++ quasar/src/pages/Index.vue
@@ -1,6 +1,13 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page padding>
+    <div class="flex flex-center">
+      <h1 class="q-display-3">Voce quer comer o que?</h1>
+    </div>
+    <div class="row gutter-md">
+      <div class="col-xs-12 col-md-4">pegar localização atual</div>
+      <div class="col-xs-12 col-md-4">acessar a conta</div>
+      <div class="col-xs-12 col-md-4">listar localizações cadastradas</div>
+    </div>
   </q-page>
 </template>
 
