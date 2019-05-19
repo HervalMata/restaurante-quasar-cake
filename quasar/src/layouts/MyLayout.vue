c6da2f7e05445159e1fcb00207ad54ea899e2862
HervalMata <hervalmataamparo@gmail.com>
2019-05-19 14:47:21 -0300

    Menu do projeto

--- quasar/src/layouts/MyLayout.vue
+++ quasar/src/layouts/MyLayout.vue
@@ -33,25 +33,22 @@
         inset-delimiter
       >
         <q-list-header>Essential Links</q-list-header>
-        <q-item @click.native="openURL('http://quasar-framework.org')">
-          <q-item-side icon="school" />
-          <q-item-main label="Docs" sublabel="quasar-framework.org" />
+        <q-item to="">
+          <q-item-side icon="search" />
+          <q-item-main label="Quero pedir" sublabel="Busque por restaurantes e faça um pedido" />
         </q-item>
-        <q-item @click.native="openURL('https://github.com/quasarframework/')">
-          <q-item-side icon="code" />
-          <q-item-main label="GitHub" sublabel="github.com/quasarframework" />
+        <q-item to="">
+          <q-item-side icon="fastfood" />
+          <q-item-main label="Tenho um restaurante"
+                       sublabel="Cadastre um restaurante e receba seus pedidos" />
         </q-item>
-        <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
-          <q-item-side icon="chat" />
-          <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg" />
+        <q-item to="">
+          <q-item-side icon="shopping_cart" />
+          <q-item-main label="Meus Pedidos" sublabel="Acompanhe seus últimos pedidos" />
         </q-item>
-        <q-item @click.native="openURL('http://forum.quasar-framework.org')">
-          <q-item-side icon="record_voice_over" />
-          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
-        </q-item>
-        <q-item @click.native="openURL('https://twitter.com/quasarframework')">
-          <q-item-side icon="rss feed" />
-          <q-item-main label="Twitter" sublabel="@quasarframework" />
+        <q-item to="">
+          <q-item-side icon="account_box" />
+          <q-item-main label="Entrar" sublabel="Acesse sua conta" />
         </q-item>
       </q-list>
     </q-layout-drawer>
